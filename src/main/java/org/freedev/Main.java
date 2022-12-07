284ff8477dbc100b55e0eab8e67fa29f5cacbee0
Mdiallo-freedev <diallo.mamadoudiallodi@gmail.com>
2022-12-07 16:53:04 +0000

    firstCommit

---
+++ src/main/java/org/freedev/Main.java
@@ -0,0 +1,9 @@
+package org.freedev;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.calculateAdd(12,13);
+    }
+}
---
+++ src/main/java/org/freedev/SimpleCalculator.java
@@ -0,0 +1,8 @@
+package org.freedev;
+
+public class SimpleCalculator {
+
+    public int calculateAdd(int num1,int num2){
+        return num1 + num2;
+    }
+}
