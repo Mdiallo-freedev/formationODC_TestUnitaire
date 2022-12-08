365a8de7c74b875f18eb0894933bf10d155146e2
Mdiallo-freedev <diallo.mamadoudiallodi@gmail.com>
2022-12-08 10:55:57 +0000

    Testunitaire

--- src/test/java/org/freedev/SimpleCalculatorTest.java
+++ src/test/java/org/freedev/SimpleCalculatorTest.java
@@ -6,28 +6,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleCalculatorTest {
-
-
+    SimpleCalculator calculator;
+    @BeforeEach
+    void init(){
+        calculator = new SimpleCalculator();
+    }
     @Test
     void test_if_calculated_is_working(){
-        SimpleCalculator calculator = new SimpleCalculator();
         assertEquals(22,calculator.addition(10,12));
-
     }
-
     @Test
-    void testSoustraction(){
-        SimpleCalculator calculator = new SimpleCalculator();
+    void test_soustraction(){
         assertEquals(4,calculator.soustraction(14,10));
     }
     @Test
-    void testMultiplication(){
-        SimpleCalculator calculator = new SimpleCalculator();
+    void test_multiplication(){
         assertEquals(10,calculator.multiplication(5,2));
     }
     @Test
-    void testBoolean(){
-        SimpleCalculator calculator = new SimpleCalculator();
+    void test_isBoolean(){
+        assertEquals(true,calculator.isEvent(10));
         assertEquals(false,calculator.isEvent(13));
     }
 
